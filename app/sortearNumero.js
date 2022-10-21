2c1ecd373241af3598c71198d46116a2fc908718
Renato Martins <rmartins6694@gmail.com>
2022-10-21 14:29:57 -0300

    add const element in sortearNumero.js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,7 +1,19 @@
+const smallestNumber = 1
+const higherNumber = 100
 const numberSecret = generateNumber();
 
-function generateNumber () {
-    return parseInt(Math.random()*100);
-}
 
+
+function generateNumber () {
+
+    return parseInt(Math.random() * higherNumber+1);
+
+}
+  const elementHigherNumber = document.getElementById('maior-valor');
+  elementHigherNumber.innerHTML = higherNumber;
+
+  const elementSmallestNumber = document.getElementById('menor-valor');
+  elementSmallestNumber.innerHTML = smallestNumber;
+
+  console.log(numberSecret)
 
